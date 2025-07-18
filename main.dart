34753431a60598b36bd3778fedbcd5983393e095
Shah Bakhsh <shahbakhshmugheri16@gmail.com>
2025-07-18 15:34:48 +0300

    Work has been Done

---
+++ main.dart
@@ -0,0 +1,93 @@
+// First Class Work Dart
+void main() {
+  // print("Hello Good AfterNoon");
+  // var stdName = "Shah Bakhsh Mugheri";
+  // print(stdName);
+  // var stdName = "Shah Bakhsh";
+  // var StdUniveristyName = "University of Sindh";
+  // var stdDob = "06/02/2002";
+  // var stdEmail = "shahbakhshmugher.it@gmail.com";
+
+  // print("Student Name: '$stdName'");
+  // print("Student University Name: '$StdUniveristyName'");
+  // print("Student data of birth: '$stdDob'");
+  // print("Studenty email id: '$stdEmail'");
+
+  // var num1 = 12;
+  // var num2 = 14;
+  // var finalResult = num1 + num2;
+
+  // print("Total number of addition '$finalResult'");
+
+  // var firstName = "Shah Bakhsh";
+  // var lastName = "Mugheri";
+  // var PhoneNo = "03136444621";
+  // var emailAddress = "shahbakhshmugheri.it@gmail.com";
+
+  // print("Enter The First Name '$firstName'");
+  // print("Enter The Last Name '$lastName'");
+  // print("Enter The Phone Number '$PhoneNo'");
+  // print("Enter The Email Address '$emailAddress'");
+
+  // 2nd Datatype Class Work Dart
+
+  // String stdName = "Shah bakhsh";
+  // print(stdName);
+  // int num1 = 12;
+  // int num2 = 4;
+  // int finalResult = num1 + num2;
+
+  // print("Total Number of Value $finalResult");
+
+  // bool isAssignmentDone = true;
+  // print(
+  //   "If Assignment Done allow to these Students otherwise leave not done Assignment $isAssignmentDone",
+  // );
+
+  // var num1 = 12;
+  // var num2 = 8;
+
+  // print(num1 > num2);
+
+  //Arthimetic Operator//
+
+  // int num1 = 10;
+  // int num2 = 3;
+
+  // print(num1 + num2);
+  // print(num1 - num2);
+  // print(num1 / num2);
+  // print(num1 * num2);
+  // print(num1 % num2);
+  // print(num1 ~/ num2);
+
+  //Equality Relation Operator//
+
+  // int num1 = 12;
+  // int num2 = 5;
+
+  // print(num1 > num2);
+  // print(num1 < num2);
+  // print(num1 >= num2);
+  // print(num1 <= num2);
+  // print(num1 == num2);
+  // print(num1 != num2);
+
+  //Logical Relation Operator//
+
+  // bool abc = true;
+  // bool xyz = false;
+  // bool mno = true;
+
+  // print(abc && mno);
+  // print(abc && mno && xyz);
+  // print(true && true && false);
+
+  bool abc = true;
+  bool xyz = false;
+  bool mno = true;
+
+  print(abc || mno || xyz);
+  print(abc && (xyz || mno));
+  print(!mno);
+}
